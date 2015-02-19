a51718710e65ed9087dffd7b64eceabd1cda6514
David Zulaica and Grace Mekarski <gracelauren909@gmail.com>
2015-02-19 14:44:03 -0800

    Create lists

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(function(){
+  $("form").submit(function(event) {
+    event.preventDefault();
 
+    var listName = $("#new-list-name").val();
+
+    var newList = {
+                    name: listName,
+                    tasks: []
+                  };
+
+    $("#lists ul").append("<li>" + newList.name + "</li>");
+    $("#lists").show();
+  });
 });
